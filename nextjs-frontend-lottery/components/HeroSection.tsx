4d0835311490e6d913cabf4b28957d05152e88ef
marwenkouidhi <marwen@kouidhi@gmail.com>
2022-09-13 11:02:37 +0100

    update hero section

--- nextjs-frontend-lottery/components/HeroSection.tsx
+++ nextjs-frontend-lottery/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { MdTimelapse } from "react-icons/md";
 const HeroSection = () => {
   const days = [];
   return (
-    <div className="font-semibold">
+    <div className="font-semibold hover:dark:brightness-105 hover:brightness-95">
       <div className="flex items-end justify-center opacity-80 dark:opacity-60 space-x-3 ">
         <div className="-mb-1">
           <Image src="/celebration1.png" height={150} width={150} />
@@ -17,8 +17,8 @@ const HeroSection = () => {
           <Image src="/celebration2.png" height={150} width={150} />
         </div>
       </div>
-      <div className=" bg-gray-300 dark:bg-slate-800 rounded-full p-5 px-10 flex justify-between items-center ">
-        <div className=" flex space-x-2">
+      <div className=" bg-gray-300 dark:bg-slate-800  rounded-full p-5 px-10 flex justify-between items-center cursor-pointer">
+        <div className=" flex space-x-4">
           <Image src="/hero3.png" height={80} width={80} />
           <div>
             <h2 className="text-xl text-yellow-500">Jackpot!</h2>
@@ -28,7 +28,8 @@ const HeroSection = () => {
             <p>USD 2,420,000</p>
           </div>
         </div>
-        <div className="flex space-x-2">
+        <div className="w-1 h-14 dark:bg-gray-100 bg-slate-900 shadow-2xl rounded-full" />
+        <div className="flex space-x-4">
           <Image src="/hero4.png" height={80} width={80} />
           <div className="flex flex-col space-y-3">
             <p className="text-base">Till the next draw</p>
@@ -45,6 +46,7 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
+        <div className="w-1 h-14 dark:bg-gray-100 bg-slate-900 shadow-2xl rounded-full" />
         <div className="flex space-x-2 items-center bg-gray-100 dark:bg-slate-700 rounded-full ">
           <p className="pl-4 uppercase">Start from 2 bets</p>
           <button className="bg-gradient-to-b from-yellow-400 to-yellow-600 p-2 px-4 rounded-full text-base">
